edbf3ca76e27c77897a82dc6a84e54c8b2e2f22b
Álvaro Gómez <gomezmansion@gmail.com>
2023-06-04 11:19:33 +0200

    search value updated

--- src/app/pipes/buscar.pipe.ts
+++ src/app/pipes/buscar.pipe.ts
@@ -8,7 +8,7 @@ export class BuscarPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     
     let [lugar] = args
-    let buscar = value.filter((municipio : any) => {
+    let buscar = value?.filter((municipio : any) => {
       return municipio.NOMBRE.toLowerCase().startsWith(lugar.toLowerCase())
     })
     if( lugar !== ''){
